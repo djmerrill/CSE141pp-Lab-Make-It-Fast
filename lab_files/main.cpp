52e26dbab45ecae5d76a2816fa7a483dc972b52f
Devon <9127315+djmerrill@users.noreply.github.com>
2019-09-05 21:27:27 -0700

    Update main.cpp

--- lab_files/main.cpp
+++ lab_files/main.cpp
@@ -74,7 +74,7 @@ int main(int argc, char * argv[]) {
         }
     }
     {
-	uint64_t size = mat_size_large/3*2;
+	uint64_t size = mat_size_large/4*5;
 	fprintf(stderr, "size=%ld\n", size);
         reference(C, A, B, size);
         sqmm(D, A, B, size);
